990fe3402b65bf9bda11380624ccb2a03f7f1901
figurebelow <ruben@figurebelow.com>
2017-11-23 08:20:10 +0000

    Code cleaning

--- technify/sample.py
+++ technify/sample.py
@@ -7,4 +7,3 @@ if __name__ == "__main__":
     s = Stock(sans[110:150]).addMa(10).addMa(20)
     s.addCrossover("ma10", "ma20", "maCross")
     s.show( "ma10", "ma20", "maCross")
-    #s.showCross("maCross")
--- technify/stock.py
+++ technify/stock.py
@@ -49,7 +49,6 @@ class Stock:
     return self
 
   def show (self, *args):
-        #plt.plot(self.data.date, self.data.c)
         colNames = []
         for colName in args:
             if not colName in self.crossOvers:
@@ -62,32 +61,4 @@ class Stock:
                 plt.scatter(low.date.values, low.ma20.values, s=165, alpha=0.6, edgecolors="b")
                 plt.scatter(up.date.values, up.ma20.values, s=165, alpha=0.6, edgecolors="g")
         plt.legend(colNames)
-
-    #p = Portfolio(s)
-    #buyHold = p.getBuyAndHold()
-    #print (buyHold)
-    #emaHold = p.getBuySellStrategy()
-    #print(emaHold)
-    #print (p.getRelativeReturn(emaHold, buyHold))
-
-
-  #ax.text(dt.date(2011, 11, 9), 10, "World",fontsize=19)
-
-  ###low = (ts.loc[(ts.shift(1)["ema50"] >  ts.shift(1)["ema400"]) &
-  ###             (ts["ema50"] < ts["ema400"])])
-  ###high = (ts.loc[(ts.shift(1)["ema50"] < ts.shift(1)["ema400"]) &
-  ###             (ts["ema50"] > ts["ema400"])])
-
-  ###plt.scatter(low.date.values, low.ema50.values, s=165, alpha=0.6, edgecolors="g")
-  ###plt.scatter(high.date.values,high.ema50.values, s=165, alpha=0.6, edgecolors="b")
-
-  #ax.add_artist(plt.Circle((dt.date(2011,10,2),7), radius=1, color='g'))
-
-  #ax.add_artist(plt.Ellipse((10, 10), (0.2,0.5), color='r'))
-  #ax.text(pd.Timestamp("2011-11-10"), 7, "!!!")
-  #ax.annotate('Test', (mdates.date2num(dt.date(2011,10,1)), 7), xytext=(25, 25),
-  #          textcoords='offset points', arrowprops=dict(arrowstyle='-|>'))
-  ###plt.show()
-  #print(ts.loc[(ts.shift(1)["ema50"] < ts.shift(1)["ema400"]) &
-  #             (ts["ema50"] > ts["ema400"])])
 
