c86e472dc0dec5d5ae27a81e05755de21e166a86
figurebelow <ruben@figurebelow.com>
2018-02-16 12:09:35 +0000

    update setup

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 setup(
   name = 'technify',
   packages = find_packages(),
-  version = '0.6.5',
+  version = '0.7.0',
   description = 'A framework to run technical analysis. Powered by pandas and ta-lib.',
   author = 'Ruben Afonso',
   author_email = 'rbfrancos@gmail.com',
   url = 'https://github.com/rubenafo/technify', 
-  download_url = 'https://github.com/rubenafo/technify/archive/0.6.5.zip',
+  download_url = 'https://github.com/rubenafo/technify/archive/0.7.0.zip',
   keywords = ['technical-analysis', 'pandas', 'finance','stock', 'ta-lib'],
   classifiers = [],
   install_requires = ["yfm", "quandl","ta-lib"]
