2808cd7666d0c53c5d5eb27db4aad2e08004a746
figurebelow <ruben@figurebelow.com>
2018-03-03 11:56:02 +0000

    Append take input cols from  a list

--- technify/stock.py
+++ technify/stock.py
@@ -36,7 +36,7 @@ class Stock:
 
     # Append indicator
     #
-    def append(self, func, *cols, saveas=[], **kwargs):
+    def append(self, func, cols, saveas=[], **kwargs):
         saveas = [saveas] if type(saveas) is not list else saveas
         outputValues = []
         arraycols = list(map(lambda c: np.asarray(self.data[c]), cols))
